b30f87b9ae03b326873244db4771c3c20eddf28f
Tianshang <liuxu2006@outlook.com>
2026-06-27 06:51:58 +0800

    v1.2.0: Update version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.tianshang.guard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
